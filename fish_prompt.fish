d1ff5b4f0cb81e6152f48801ec9369923d6a521b
Hare <hare.02.k@gmail.com>
2024-05-15 01:02:26 +0900

    init

---
+++ fish_prompt.fish
@@ -0,0 +1,42 @@
+function preexec --on-event fish_preexec
+  set -g _play_command_begin_time (date +%s%3N)
+end
+
+function postexec --on-event fish_postexec
+  if test -z $_play_command_begin_time
+    return
+  end
+
+  set -l code
+  if test $status -eq 0
+    set_color green
+    set code "✔success:$status"
+  else
+    set_color red
+    set code "✘error:$status"
+  end
+
+  set -l took_time
+  set -l millis
+
+  set millis (echo (date +%s%3N)"-$_play_command_begin_time" | bc)
+  if test $millis -ge 10000
+    set took_time (echo "$millis/1000" | bc)s
+  else if test $millis -ge 1000
+    set took_time (echo "scale=2; $millis/1000" | bc)s
+  else
+    set took_time "$millis""ms"
+  end
+
+  echo -s -n $code\ (set_color magenta)\ 󱦟$took_time\n
+end
+
+function fish_prompt
+  set now_date (set_color cyan)(date +%H:%M:%S)
+  set who_n_where (set_color yellow)$USER::(prompt_hostname)
+  set pwd (prompt_pwd)
+  set enter_mark (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯'
+
+  echo -e -s -n -- $now_date\ $who_n_where\ (set_color white)\|(set_color yellow)\ $pwd\n$enter_mark\ 
+  set_color normal
+end
